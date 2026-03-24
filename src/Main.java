0a39daf8208445de006d6895b0ecba5c09403e65
MaSniezek19 <m.sniezek12@yahoo.com>
2026-03-24 10:18:17 +0100

    Zadanie 1: Samochod

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Samochod s1 = new Samochod();
+        s1.marka = "Ford";
+        s1.model = "Focus";
+        s1.rokProdukcji = 2010;
+
+        Samochod s2 = new Samochod();
+        s2.marka = "Toyota";
+        s2.model = "Corolla";
+        s2.rokProdukcji = 2020;
+
+        s1.wyswietlInfo();
+        s2.wyswietlInfo();
+    }
+}
